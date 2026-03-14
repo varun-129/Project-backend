b5bac99b1bb1d4f37f4ecb3b58aa16f28c439353
varun-129 <varun.kumar_ug23@nsut.ac.in>
2026-03-15 01:16:47 +0530

    add user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,50 @@
+import mongoose,{Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videFile: {
+            type: String, //cloudinary url
+            required: true,
+        },
+        thumbnail: {
+            type: String, //cloudinary url
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            default: false,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
