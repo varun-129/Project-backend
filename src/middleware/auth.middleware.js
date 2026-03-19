89211959e66b89eb4c51f84bc30ec0c367799770
varun-129 <varun.kumar_ug23@nsut.ac.in>
2026-03-20 02:27:38 +0530

    bug fix

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,6 +1,6 @@
 import { ApiError } from "../utils/ApiError.js";
-import asyncHandler from "express-async-handler";
-import User from "../models/user.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import {User} from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
