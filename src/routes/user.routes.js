169f9d2243e2ac21009b24260c5fa66449d6d77c
varun-129 <varun.kumar_ug23@nsut.ac.in>
2026-03-23 00:42:48 +0530

    feat: add user account management routes for password change, profile update, and avatar/cover image updates

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser ,refreshToken } from "../controllers/user.controller.js";
+import { registerUser,
+    loginUser ,
+    logoutUser ,
+    refreshToken
+ } from "../controllers/user.controller.js";
 import { upload } from '../middleware/multer.middleware.js';
 import { verifyJWT } from "../middleware/auth.middleware.js";
+import { get } from "mongoose";
 
 const router = Router();
 
@@ -27,7 +32,19 @@ router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshToken)
 
+router.route("/change-password").post(verifyJWT, changePassword)
 
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-profile").patch(verifyJWT,updateAccountDetails)
+
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar)
+
+router.route("/update-cover").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+
+router.route("/channel/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
 
 
